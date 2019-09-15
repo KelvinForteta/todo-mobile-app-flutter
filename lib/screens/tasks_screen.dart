46d3406766817c035cd6b7c24c7756bbee037f6c
Kelvin Forteta <kelvin.forteta@gmail.com>
2019-09-15 08:00:37 +0100

    Refactor the TasksScreen

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todoey_flutter/widgets/task_list.dart';
 
 class TasksScreen extends StatelessWidget {
   @override
@@ -57,12 +58,14 @@ class TasksScreen extends StatelessWidget {
           ),
           Expanded(
             child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
               decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.only(
                     topLeft: Radius.circular(20.0),
                     topRight: Radius.circular(20.0),
                   )),
+              child: TasksList(),
             ),
           )
         ],
