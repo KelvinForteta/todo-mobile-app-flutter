a4688ed060bea75c07f9d9e8018478cecdd18cdd
Kelvin Forteta <kelvin.forteta@gmail.com>
2019-09-15 07:06:39 +0100

    Initial commit

---
+++ lib/screens/tasks_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TasksScreen extends StatefulWidget {
+  @override
+  _TasksScreenState createState() => _TasksScreenState();
+}
+
+class _TasksScreenState extends State<TasksScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
